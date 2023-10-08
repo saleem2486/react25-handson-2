7e3cf58ed6bdaf095d1dd387b5f7489f2f696555
Malik Saleem <maliksaleem32596@gmail.com>
2023-10-08 20:54:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+
+
+const App = () => {
+  const [allFormData,setAllFormData]=useState([])
+const[formData,setFormData]=useState(
+  {
+    name:"",
+    department:"",
+    rating:""
+  }
+);
+const handleChange=(event)=>{
+  setFormData({...formData,[event.target.name]:event.target.value})
+  // console.log(formData)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{fontFamily:'cursive'}}>EMPLOYEE FEEDBACK FORM</h1>
+      <form action=''>
+        <label> Name: <input type='text' name="name"  onChange={handleChange} ></input></label> <br/><br/>
+        <label> Department: <input type='text' name="department" onChange={handleChange}></input></label> <br/><br/>
+        <label> Rating: <input type='number' name="rating" onChange={handleChange} ></input></label> <br/> <br/>
+        
+
+
+        <button 
+        type='button'
+        onClick={(e)=>{
+          
+          const tempObj=[...allFormData];
+          tempObj.push(formData)
+          setAllFormData(tempObj)
+          console.log("all form data",allFormData)
+
+          }}>Submit</button>
+      </form>
+      <br/> <br/>
+      <div className='Container'>
+      {
+        allFormData.map(
+          (value,index)=>{
+            return(
+              <div className='Child' key={index}>Name:{value.name}| Department:{value.department} |Rating:{value.rating}</div>
+            )
+
+          }
+        )
+      }
+
+      </div>
+      
     </div>
   );
 }
